8608896a514a80e6d392174549ebede31bcca22d
suzan-sheikh <mdsuzanskh@gmail.com>
2026-04-26 12:00:00 +0600

    old task start

--- src/task.ts
+++ src/task.ts
@@ -10,13 +10,8 @@ type CartItem = {
 const calculateTotal = ({ price, quantity = 1 }: CartItem): number =>
   price * quantity;
 
-console.log(calculateTotal({ name: "suzan", price: 10, quantity: 10 }));
-console.log(calculateTotal({ name: "suzan", price: 20 }));
-
 //* Example-2
 function calculateTotal2(item: CartItem): number{
     const {price, quantity = 1} = item;
     return price * quantity;
 }
-
-console.log(calculateTotal2({name: 'suzan' ,price: 10, }));
