b093c6d029370ac5ea584319bb78f65c3cdfc06e
suzan-sheikh <mdsuzanskh@gmail.com>
2026-04-27 12:00:00 +0600

    task 2 Megging User Pfofiles write two type

--- src/task.ts
+++ src/task.ts
@@ -9,11 +9,17 @@ type CartItem = {
 //* Example-1
 const calculateTotal = ({ price, quantity = 1 }: CartItem): number =>
   price * quantity;
-console.log(calculateTotal({name: 'suzan', price: 10, quantity: 10}));
+console.log(calculateTotal({ name: "suzan", price: 10, quantity: 10 }));
 
 //* Example-2
-function calculateTotal2(item: CartItem): number{
-    const {price, quantity = 1} = item;
-    return price * quantity;
+function calculateTotal2(item: CartItem): number {
+  const { price, quantity = 1 } = item;
+  return price * quantity;
 }
-console.log(calculateTotal2({name: 'suzan', price: 10, quantity: 10}));
+console.log(calculateTotal2({ name: "suzan", price: 10, quantity: 10 }));
+
+//* Task 2: Merging User Profiles
+
+type Person = {name: string; age: number};
+type JobDetails = {role: string; salary: number};
+
